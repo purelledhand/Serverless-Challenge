e84ca817d254942e9b0219027e01fd25a5b5c714
SoHye Choi <wergreat10@naver.com>
2019-12-17 00:07:07 +0900

    Hide API Key

--- yes-or-no/src/App.js
+++ yes-or-no/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import API_KEY from './config';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+  const [result, setResult] = useState(null);
 
-export default App;
+  useEffect(() => {
+    function getResult() {
+      fetch(`https://helloworld-20191216.azurewebsites.net/api/HttpTrigger1?&code=${API_KEY}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setResult(json.ans);
+        return json.ans;
+      });
+    }
+    getResult();
+  }, []);
+  
+  return (
+    <div className={result==="Yes"?"app green":"app"}>
+      <div className="result-text">
+        {result?result:"Loading..."}
+      </div>
+    </div>
+    );
+  }
+  
+  export default App;
